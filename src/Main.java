0138e19f9ccf709b8a1b62d4aa2807c3ec3a731e
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 11:07:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en text med hjälp av en eller flera Scanner objekt och räkna:
+        //   - Antal rader
+        //   - Antal ord
+        //   - Antal tecken
+
+
+    }
+}
