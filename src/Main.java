919520cff9099662207bd6912502de9abf5ac3bc
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 11:26:27 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in en text med hjälp av en eller flera Scanner objekt och räkna:
@@ -5,6 +7,23 @@ public class Main {
         //   - Antal ord
         //   - Antal tecken
 
+        int rows = 0;
+        int words = 0;
+        int characters = 0;
 
+        Scanner rowsScanner = new Scanner(System.in);
+        while (rowsScanner.hasNextLine()) {
+            String row = rowsScanner.nextLine();
+            rows++;
+            characters += row.length();
+
+            Scanner wordScanner = new Scanner(row);
+            while (wordScanner.hasNext()) {
+                wordScanner.next();
+                words++;
+            }
+        }
+
+        System.out.format("%8d%8d%8d", rows, words, characters);
     }
 }
